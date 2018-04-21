a17948a24eee142c8ceb09cc96a217ee47e3634f
ingsocOrg <ingsoc.org@gmail.com>
2018-04-21 11:48:21 +0530

    initial commit

---
+++ app/src/main/java/com/batman/picasso/MainActivity.java
@@ -0,0 +1,25 @@
+package com.batman.picasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    String url="http://www.hottoys.com.hk/photos/PD1483440804E0M.jpg";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.image);
+
+        Picasso.with(this)
+                .load(url)
+                .fit()
+                .into(imageView);
+    }
+}
